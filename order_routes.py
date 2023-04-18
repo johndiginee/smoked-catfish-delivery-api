a24ce9df61ed9f5b8e7e25b240cd1b4f00825ef3
John Diginee <Johndbizz@gmail.com>
2023-04-18 15:37:45 +0100

    Implemented place new order

--- order_routes.py
+++ order_routes.py
@@ -1,4 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status
+from fastapi.exceptions import HTTPException
+from fastapi_jwt_auth import AuthJWT
+from models import User, Order
+from schemas import OrderModel
+from database import Session, engine
+from fastapi.encoders import jsonable_encoder
 
 # Specify route URL
 order_router=APIRouter(
@@ -6,6 +12,50 @@ order_router=APIRouter(
     tags=['orders']
 )
 
+session = Session(bind=engine)
+
 @order_router.get('/')
-async def hello():
+async def hello(Authorize:AuthJWT=Depends()):
+
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invaid Token"
+        )
     return {"message":"Hello World 2"}
+
+
+@order_router.post('/order', status_code=status.HTTP_201_CREATED)
+async def place_an_order(order:OrderModel, Authorize:AuthJWT=Depends()):
+    """Placing an order route"""
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invaid Token"
+        )
+
+    current_user = Authorize.get_jwt_subject()
+
+    user = session.query(User).filter(User.username==current_user).first()
+
+    new_order = Order(
+        catfish_size = order.catfish_size,
+        quantity = order.quantity
+    )
+
+    new_order.user=user
+    session.add(new_order)
+    session.commit()
+
+    response = {
+        "catfish_size": new_order.catfish_size,
+        "quantity": new_order.quantity,
+        "id": new_order.id,
+        "order_status": new_order.order_status
+    }
+
+    return jsonable_encoder(response)
--- schemas.py
+++ schemas.py
@@ -31,5 +31,24 @@ class Settings(BaseModel):
 
 
 class LoginModel(BaseModel):
+    """User login"""
     username:str
     password:str
+
+
+class OrderModel(BaseModel):
+    """New order"""
+    id:Optional[int]
+    quantity:int
+    order_status:Optional[str]="PENDING"
+    catfish_size:Optional[str]="SMALL"
+    user_id:Optional[int]
+
+    class Config:
+        orm_mode = True
+        schema_extra = {
+            "example": {
+                "quantity": 2,
+                "catfish_size": "LARGE"
+            }
+        }
